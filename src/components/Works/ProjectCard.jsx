fafca5cc26b06eb1dabcffad3b46b15fb11e4a11
danilo-mello <dmello19@gmail.com>
2021-06-18 10:37:23 -0700

    fix contact page warning

--- src/components/Works/ProjectCard.jsx
+++ src/components/Works/ProjectCard.jsx
@@ -15,7 +15,7 @@ const ProjectsCard = (props) => {
                         
                         <h1>Harvest Vase</h1>
                         <h2>by studio and friends</h2>
-                        <p>Harvest Vases are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth. </p>
+                        <p>Harvest Vas es are a reinterpretation of peeled fruits and vegetables as functional objects. The surfaces appear to be sliced and pulled aside, allowing room for growth. </p>
                     </div>
                     <div className="product-price-btn">
                         <p><span>78</span>$</p>
